2f0ca22f7fccaa1c0ee9fc5b726f714be8195eb5
SylviaWhittle <sylviwhittle@gmail.com>
2021-09-13 15:51:12 +0100

    Update Project 8.js

--- Project 8/Project 8.js
+++ Project 8/Project 8.js
@@ -6,12 +6,19 @@ let boidMaxSpeed = 3;
 let boidSeparationDist = 25.0;
 let boidAlignmentDist = 60;
 let boidCohesionDist = 50;
-let separationForce = 1;
+let separationForce = 1.5;
 let alignmentForce = 1;
 let cohesionForce = 1;
 
-gui = new dat.GUI();
-gui.add(window, "maxSpeedInitial", 0, 50).step(1);
+let boidMaxForce = 0.05;
+
+let obstacleSize = 100;
+let avoidanceForce = 0.5;
+let obstacleMaxForce = 0.05;
+let boidObstacleDetectionDist = 200;
+
+//gui = new dat.GUI();
+//gui.add(window, "maxSpeedInitial", 0, 50).step(1);
 // gui.add(window, "boidMaxSpeed", 0, 100).step(1);
 // gui.add(window, "boidSeparationDist", 0, 100).step(1);
 
@@ -27,13 +34,26 @@ function setup() {
     flock = new Flockmanager();
   
     // Create boids
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 100; i++) {
         let b = new Boid(width / 2,height / 2);
         flock.createBoid(b);
     }
+
+    // Create obstacles
+    // for (let i = 0; i < 10; i++) {
+    //     let obstacle = new Obstacle( 50 + 40 * i, 50, obstacleSize);
+    //     flock.createObstacle(obstacle);
+    // }
+
+    // for (let i = 0; i < 10; i++) {
+    //     let obstacle = new Obstacle(50 + 40 * i, 300, obstacleSize);
+    //     flock.createObstacle(obstacle);
+    // }
+    //let obstacle = new Obstacle(100, 100, obstacleSize);
+    //flock.createObstacle(obstacle);
 }
 
-// The main draw function
+// The main draw function - acts as the main script
 function draw() {
     //background(51);
     clear();
@@ -47,14 +67,19 @@ function mouseClicked() {
 
 function Flockmanager() {
     this.boids = [];
+    this.obstacles = [];
 }
 
-// 
+// Main flock update script called by draw
 Flockmanager.prototype.run = function() {
-    for (let i = 0; i < this.boids.length; i++) {
-    // Tell each boid to update, according to the list of all boids that the manager has
-    this.boids[i].run(this.boids);  
+        for (let i = 0; i < this.boids.length; i++) {
+        // Tell each boid to update, according to the list of all boids that the manager has
+        this.boids[i].run(this.boids, this.obstacles);  
   }
+
+  for (let i = 0; i < this.obstacles.length; i++) {
+        this.obstacles[i].draw();
+    }
 }
 
 // Add new boid to the simulation
@@ -62,11 +87,38 @@ Flockmanager.prototype.createBoid = function(b) {
     this.boids.push(b);
 }
 
+// Add new obstacle to the simulation
+Flockmanager.prototype.createObstacle = function(obstacle) {
+    this.obstacles.push(obstacle);
+}
+
+// Obstacle function
+function Obstacle(x, y, size) {
+    this.position = createVector(x, y);
+    this.size = size;
+}
+
+Obstacle.prototype.draw = function() {
+    // Draw style settings for boid
+    fill(50, 50, 50, 50);
+    stroke(10, 10, 10, 40);
+    push();
+  
+    //translate(this.position.x, this.position.y);
+    beginShape();
+    circle(this.position.x, this.position.y, this.size/2);
+    endShape(CLOSE);
+
+    // Reset draw settings
+    pop();
+}
+
 // Boid function
 function Boid(x, y) {
     //// Navigation stats
     // Maximum force for turning
-    this.maxforce = 0.05; 
+    this.maxforce = boidMaxForce; 
+    this.obstacleMaxForce = obstacleMaxForce;
     // Boid maximum speed
     this.maxspeed = boidMaxSpeed;
 
@@ -80,29 +132,34 @@ function Boid(x, y) {
 }
 
 // Boid main script
-Boid.prototype.run = function(boids) {
+Boid.prototype.run = function(boids, obstacles) {
     // Update acceleration
-    this.update(boids);
+    this.update(boids, obstacles);
     this.drawBoid();
 }
 
 // Update the acceleration based on the flocking rules
-Boid.prototype.update = function(boids) {
+Boid.prototype.update = function(boids, obstacles) {
     // Separation
     let separation = this.separate(boids);   
     // Alignment
     let alignment = this.align(boids);      
     // Cohesion
     let cohesion = this.cohere(boids);   
+
+    // Avoidance of obstacles
+    let avoidance = this.avoid(obstacles);
   
     // Scale the values to make them play nicely
     separation.mult(separationForce);
     alignment.mult(alignmentForce);
     cohesion.mult(cohesionForce);
+    avoidance.mult(avoidanceForce);
     // Add the force vectors to acceleration
     this.acceleration.add(separation);
     this.acceleration.add(alignment);
     this.acceleration.add(cohesion);
+    this.acceleration.add(avoidance);
 
     // Perhaps add mass? could make size propto mass? Acceleration = Force / Mass
 
@@ -180,6 +237,59 @@ Boid.prototype.separate = function(boids) {
         turnTo.mult(this.maxspeed);
         turnForce = turnTo.sub(this.velocity);
         turnForce.limit(this.maxforce);
+    }
+    return turnTo;
+}
+
+// Avoid objects 
+Boid.prototype.avoid = function(obstacles) {
+
+    // Turning vector
+    let turnTo = createVector(0, 0);
+    let turnForce = createVector(0, 0);
+    // Closeby boid counter for averaging
+    let count = 0;
+
+    let closestDistance = 100000000;
+
+    // Find boids that are too close
+    for (let i = 0; i < obstacles.length; i++) {
+        // Get distance to edge of the obstacle center
+        let distance = p5.Vector.dist(this.position,obstacles[i].position);
+        // Subtract the radius to get the distance to the edge
+        //distance = distance - obstacles[i].size;
+        // If the boid is closeby and not yourself (0 is returned if the boid is checking itself)
+        if ((distance > 0) && (distance < boidObstacleDetectionDist)) {
+            // Get displacement to the center
+            let displacement = p5.Vector.sub(this.position, obstacles[i].position);
+            // Subtract the vector from the center to the edge of the circle from the displacement to get the
+            // distance to the edge of the circle.
+            //displacement.sub(displacement.normalize().mult(obstacles[i].size));
+            displacement.normalize();
+            // Reduce for the distance
+            displacement.div(distance); 
+            // Set the vector to turn to
+            turnTo.add(displacement);
+            count++;            
+
+            // Keep track of closest distance to obstacle (for increasing the force later on)
+            if(distance < closestDistance) {
+                closestDistance = distance;
+            }
+        }
+    }
+    // Take an average of the turn strength based on how many boids there are closeby
+    if (count > 0) {
+        turnTo.div(count);
+    }
+
+    if (turnTo.mag() > 0) {
+        // Turning force = desired vector - velocity
+        turnTo.normalize();
+        turnTo.mult(this.maxspeed);
+        turnForce = turnTo.sub(this.velocity);
+        // Limit the force propto the distance away from closest obstacle
+        turnForce.limit(this.obstacleMaxForce);
     }
     return turnTo;
 }
